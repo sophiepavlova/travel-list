0c1a11fef2a0f049c014350d71fce022533aeb74
sophiepavlova <olni.pavlova@gmail.com>
2026-03-29 21:01:49 -0700

    refactor: move components into separate files

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Logo from './Logo.js';
+import Form from './Form.js';
+import PackingList from './PackingList.js';
+import Stats from './Stats.js';
+
+// const initialItems = [
+//   { id: 1, description: 'Passports', quantity: 2, packed: false },
+//   { id: 2, description: 'Socks', quantity: 12, packed: false },
+//   { id: 3, description: 'Charger', quantity: 1, packed: true },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+    console.log(items);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((i) => i.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
+    );
+  }
+
+  function handleClearItems() {
+    const confirmed = window.confirm(
+      'Do you want to delete all the items in the list?',
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className='app'>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
